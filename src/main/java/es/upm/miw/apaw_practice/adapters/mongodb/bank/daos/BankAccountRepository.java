14f81a20f38362caee96aa313141e45ec6b16efd
[jesus romero] <jesus.romerovidal@gmail.com>
2025-03-21 18:53:28 +0100

    add concrete methods to daos #3

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/daos/BankAccountRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/daos/BankAccountRepository.java
@@ -1,9 +1,12 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.bank.daos;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.bank.entities.BankAccountEntity;
 
 public interface BankAccountRepository extends MongoRepository<BankAccountEntity, String> {
 
+    Optional<BankAccountEntity> findByIban(String iban);
 }
--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/daos/InvestmentFundRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/daos/InvestmentFundRepository.java
@@ -1,9 +1,12 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.bank.daos;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.bank.entities.InvestmentFundEntity;
 
 public interface InvestmentFundRepository extends MongoRepository<InvestmentFundEntity, String> {
 
+    Optional<InvestmentFundEntity> findByName(String name);
 }
