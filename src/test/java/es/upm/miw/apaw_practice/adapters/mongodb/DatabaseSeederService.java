f1e29a55afebb4a54099dad8e8a3626ecdce722d
[jesus romero] <jesus.romerovidal@gmail.com>
2025-03-21 20:57:45 +0100

    add Bank database seeder #3

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.bank.BankSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,19 +9,23 @@ import org.springframework.stereotype.Service;
 public class DatabaseSeederService {
 
     private final ShopSeederService shopSeederService;
+    private final BankSeederService bankSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, BankSeederService bankSeederService) {
         this.shopSeederService = shopSeederService;
+        this.bankSeederService = bankSeederService;
         this.seedDatabase();
     }
 
     public void seedDatabase() {
         this.shopSeederService.seedDatabase();
+        this.bankSeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
+        this.bankSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
